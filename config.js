8c0c74b46cf0bcf655327fd1f9f8d932172b140f
mihchis <trin79136@gmail.com>
2021-08-15 20:29:30 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin chào, mihchis nè!',
+    introDesc: `Tui có điều này muốn hỏi bà, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Hết dịch tụi mình cùng nhau đi ăn nhaaaaaaaa 😙',
+    desc: 'Không trả lời hoặc thoát ra, tức là đồng ý đi ăn với tui rồi đó nha :> ',
+    btnYes: 'oke :3',
+    btnNo: 'đéo :)))',
+    question:'Hứa ở đây điiiiii!',
+    btnReply: 'Xong rùi đó :v',
+    reply: 'Tui mà ko đi ăn với mihchis là mihchis bỏ đi du học suốt đời luônnnnn !!!',
+    mess: 'Áhihi kiểu gì cũng phải đồng ý thoi :)))',
+    messDesc: 'giờ thì nhắn tin hẹn lịch chốt kèo với nhau đi :v',
+    btnAccept: 'Okiiiii lun :v',
+    messLink: 'https://m.me/mihchis'
+}
